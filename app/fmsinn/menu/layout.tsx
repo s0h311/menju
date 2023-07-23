ed584ea4b60122f6e3e5db35fec44ef04bbb1713
maxiirs <maximilian.buchmann@haw-hamburg.de>
2023-07-23 11:57:01 +0200

    linting

--- app/fmsinn/menu/layout.tsx
+++ app/fmsinn/menu/layout.tsx
@@ -1,12 +1,10 @@
-import {TrpcProvider} from "@/trpc/trpc-provider";
-import {Container} from "@mui/material";
+import { TrpcProvider } from '@/trpc/trpc-provider'
+import { Container } from '@mui/material'
 
-export default function MenuLayout({children}: { children: React.ReactNode }) {
-    return (
-        <TrpcProvider>
-            <Container>
-                {children}
-            </Container>
-        </TrpcProvider>
-    )
+export default function MenuLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <TrpcProvider>
+      <Container>{children}</Container>
+    </TrpcProvider>
+  )
 }
