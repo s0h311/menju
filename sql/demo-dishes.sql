34d9c43cec5c59a5382bda4e9bebf0c54794cac2
Soheil Nazari <kontakt@soheilnazari.de>
2023-07-25 21:06:48 +0200

    fix language

--- sql/demo-dishes.sql
+++ sql/demo-dishes.sql
@@ -2,14 +2,14 @@ INSERT INTO public."Restaurant" (name) VALUES ('Batman');
 
 INSERT INTO public."DishCategory" (name, picture, "restaurantId") 
   SELECT
-  '{ "en": "lunch", "de": "mittagessen", "it": "pranzo" }',
+  '{ "EN": "lunch", "DE": "mittagessen", "IT": "pranzo" }',
   'https://lenamerz.de/wp-content/uploads/2021/03/schnelle-mittagessen-fuers-homeoffice.jpg',
   "Restaurant".id
 FROM public."Restaurant" ORDER BY id DESC;
 
 INSERT INTO public."DishCategory" (name, picture, "restaurantId") 
   SELECT
-  '{ "en": "dinner", "de": "abendessen", "it": "cena" }',
+  '{ "EN": "dinner", "DE": "abendessen", "IT": "cena" }',
   'https://images.lecker.de/leichtes-abendessen-b15jpg,id=ef48bb96,b=lecker,w=1600,rm=sk.jpeg',
   "Restaurant".id
 FROM public."Restaurant" ORDER BY id DESC;
@@ -17,27 +17,27 @@ FROM public."Restaurant" ORDER BY id DESC;
 INSERT INTO public."Dish" ("categoryId", name, price, picture, ingredients)
   SELECT
 	"DishCategory".id,
-	'{ "en": "Doner kebab", "de": "Döner", "it": "Kebab" }',
+	'{ "EN": "Doner kebab", "DE": "Döner", "IT": "Kebab" }',
 	6.50,
 	'https://www.blog.vegan-masterclass.de/wp-content/uploads/2021/09/Doener_mit_gegrillter_Paprika.jpg',
-	'{ "en": ["Meat", "Bread", "Salat", "Sauce"], "de": ["Fleisch", "Brot", "Salat", "Soße"], "it": ["Carne", "Pane", "Insalata", "Salsa"] }'
+	'{ "EN": ["Meat", "Bread", "Salat", "Sauce"], "DE": ["Fleisch", "Brot", "Salat", "Soße"], "IT": ["Carne", "Pane", "Insalata", "Salsa"] }'
 FROM public."DishCategory" ORDER BY id DESC LIMIT 1;
 
 
 INSERT INTO public."Dish" ("categoryId", name, price, picture, ingredients)
   SELECT
   "DishCategory".id,
-  '{ "en": "Pasta", "de": "Nudeln", "it": "Pasta" }',
+  '{ "EN": "Pasta", "DE": "Nudeln", "IT": "Pasta" }',
   9.30,
   'https://www.eatbetter.de/sites/eatbetter.de/files/styles/facebook/public/2021-07/nudeln_mit_kichererbsen_1.jpg?h=4521fff0&itok=C9tS61Im',
-  '{ "en": ["Pasta", "Parmesan", "Tomato sauce"], "de": ["Nudeln", "Parmesan", "Tomatensoße"], "it": ["Pasta", "Parmigiano", "Salsa di pomodoro"] }'
+  '{ "EN": ["Pasta", "Parmesan", "Tomato sauce"], "DE": ["Nudeln", "Parmesan", "Tomatensoße"], "IT": ["Pasta", "Parmigiano", "Salsa di pomodoro"] }'
 FROM public."DishCategory" ORDER BY id DESC LIMIT 1;
 
 INSERT INTO public."Dish" ("categoryId", name, price, picture, ingredients)
   SELECT
 	"DishCategory".id,
-	'{ "en": "Pizza", "de": "Pizza", "it": "Pizza" }',
+	'{ "EN": "Pizza", "DE": "Pizza", "IT": "Pizza" }',
 	9.30,
 	'https://www.hamburg.de/image/15536634/kingTeaser/990/420/d4e83a1a6365d5c6804a9bf2ccef7fbc/RS/symbolbild-pizza-bild.jpg',
-	'{ "en": ["Dough", "Cheese", "Tomato sauce"], "de": ["Teig", "Käse", "Tomatensoße"], "it": ["Impasto", "Formaggio", "Salsa di pomodoro"] }'
+	'{ "EN": ["Dough", "Cheese", "Tomato sauce"], "DE": ["Teig", "Käse", "Tomatensoße"], "IT": ["Impasto", "Formaggio", "Salsa di pomodoro"] }'
 FROM public."DishCategory" ORDER BY id DESC LIMIT 1;
