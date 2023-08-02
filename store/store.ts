a50312f9f9fba3cd4ced175dd0e622fce73b640c
Soheil Nazari <kontakt@soheilnazari.de>
2023-08-02 20:24:20 +0200

    FMS-20 added persistence

--- store/store.ts
+++ store/store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 import { Cart, OrderPosition } from '@/app/types/order.type'
 
 type CartState = {
@@ -7,23 +8,30 @@ type CartState = {
   addPosition: (position: OrderPosition) => void
 }
 
-export const useCartStore = create<CartState>((set) => ({
-  cart: {
-    table: '',
-    positions: [],
-    paymentMethod: 'CARD',
-    isPayed: false,
-    netTotal: 0,
-    restaurantId: 0,
-    vat: null,
-    note: null,
-  },
-  setCart: (cart: Cart) => set(() => ({ cart: cart })),
-  addPosition: (position: OrderPosition) =>
-    set((state) => ({
+export const useCartStore = create(
+  persist<CartState>(
+    (set, get) => ({
       cart: {
-        ...state.cart,
-        positions: [...state.cart.positions, position],
+        table: '',
+        positions: [],
+        paymentMethod: 'CARD',
+        isPayed: false,
+        netTotal: 0,
+        restaurantId: 0,
+        vat: null,
+        note: null,
       },
-    })),
-}))
+      setCart: (cart: Cart) => set(() => ({ cart: cart })),
+      addPosition: (position: OrderPosition) =>
+        set((state) => ({
+          cart: {
+            ...get().cart,
+            positions: [...get().cart.positions, position],
+          },
+        })),
+    }),
+    {
+      name: 'cart',
+    }
+  )
+)
