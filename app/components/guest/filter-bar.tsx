cd5886632c7e68c5f4bb0b7fc2dec67fbd8d82ff
maxiirs <maximilian.buchmann@haw-hamburg.de>
2023-08-16 00:31:02 +0200

    wip

--- app/components/guest/filter-bar.tsx
+++ app/components/guest/filter-bar.tsx
@@ -2,13 +2,23 @@ import { Chip, Stack } from '@mui/material'
 import React from 'react'
 import { FilterChipModel } from '@/app/types/filter-chip.types'
 import { useMenuStore } from '@/store/menu-store'
+import useStore from '@/store/nextjs-hook'
 
 type FilterBarProps = {
   chipData: FilterChipModel[]
 }
 
 export default function FilterBar({ chipData }: FilterBarProps) {
-  const menuStore = useMenuStore((state) => state)
+  const menuStore = useStore(useMenuStore, (state) => state)
+  function isFilterActive(filterLabel: string): Boolean {
+    var isFilterActive = false
+    menuStore?.activeFilter.map((filter) => {
+      if (filter.label === filterLabel) {
+        isFilterActive = true
+      }
+    })
+    return isFilterActive
+  }
 
   return (
     <Stack
@@ -18,11 +28,14 @@ export default function FilterBar({ chipData }: FilterBarProps) {
     >
       {chipData.map((filter: FilterChipModel) => (
         <Chip
+          variant='outlined'
           key={filter.label}
           label={filter.label}
+          color={isFilterActive(filter.label) ? 'success' : 'error'}
+          style={{ backgroundColor: '#fcfaf2' }}
           onClick={() => {
-            menuStore.updateFilter(filter)
-            menuStore.filter()
+            menuStore?.updateFilter(filter)
+            menuStore?.filter()
           }}
         />
       ))}
