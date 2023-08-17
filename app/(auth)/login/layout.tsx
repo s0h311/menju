b825f90341bd6b363d2cac3a5fe4da5ae767d9c6
Soheil Nazari <kontakt@soheilnazari.de>
2023-08-17 17:08:54 +0200

    Middleware für login, cleanup

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -4,14 +4,14 @@ export const metadata: Metadata = {
   title: 'Einloggen | FMSinn',
 }
 
-type RootLayoutProps = {
+type LoginLayoutProps = {
   children: React.ReactNode
 }
 
-export default function LoginLayout({ children }: RootLayoutProps) {
+export default function LoginLayout({ children }: LoginLayoutProps) {
   return (
     <div className='grid place-items-center h-screen'>
-      <div className='w-[90dvw] xl:w-[70dvw] h-[90dhv] xl:h-[80dvh]'>{children}</div>
+      <div className='w-[90dvw] xl:w-[70dvw] h-[90dvh] xl:h-[80dvh]'>{children}</div>
     </div>
   )
 }
