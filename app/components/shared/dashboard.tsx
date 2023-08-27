ae15c66eac5c2bb5103ed4a07f71ee80c6e79aa2
Soheil Nazari <kontakt@soheilnazari.de>
2023-08-27 10:04:17 +0200

    FMS-33 Dashboard Layout angepasst

--- app/components/shared/dashboard.tsx
+++ app/components/shared/dashboard.tsx
@@ -3,7 +3,6 @@
 import { ReactNode } from 'react'
 import Sidebar from './sidebar'
 import { SidebarMenu } from '@/app/types/dashboard.type'
-import { usePathname } from 'next/navigation'
 
 type DashboardProps = {
   sidebarMenus: SidebarMenu[]
@@ -11,18 +10,11 @@ type DashboardProps = {
 }
 
 export default function Dashboard({ sidebarMenus, children }: DashboardProps) {
-  const path = usePathname()
-  const title = sidebarMenus.find((menu) => menu.path === path)?.title
-
   return (
-    <main className='grid place-items-center h-screen w-screen p-10'>
-      <section className='flex gap-10 w-full h-full'>
+    <main className='grid place-items-center h-screen w-screen'>
+      <section className='flex w-full h-full'>
         <Sidebar menus={sidebarMenus} />
-        <div className='space-y-2 w-full h-full rounded-lg'>
-          <h1 className='text-lg'>{title}</h1>
-          <hr className='border-[0.5px] border-gray-300' />
-          {children}
-        </div>
+        <div className='space-y-3 w-full h-full rounded-lg p-5 xl:p-10 '>{children}</div>
       </section>
     </main>
   )
--- app/components/shared/sidebar.tsx
+++ app/components/shared/sidebar.tsx
@@ -1,6 +1,7 @@
 import { SidebarMenu } from '@/app/types/dashboard.type'
 import Link from 'next/link'
 import { useState } from 'react'
+import { usePathname } from 'next/navigation'
 
 type SidebarProps = {
   menus: SidebarMenu[]
@@ -8,40 +9,38 @@ type SidebarProps = {
 
 export default function Sidebar({ menus }: SidebarProps) {
   const [showSidebar, setShowSidebar] = useState<boolean>(true)
-
+  const path = usePathname()
   return (
     <>
-      {!showSidebar ? (
+      {!showSidebar && (
         <button
-          className='absolute top-10 left-0 bg-slate-400 px-1.5 py-5 rounded-r-md text-white'
+          className='absolute top-5 left-0 bg-accent px-1.5 py-5 rounded-r-md'
           onClick={() => setShowSidebar(!showSidebar)}
         >
           {'>>'}
         </button>
-      ) : (
-        ''
       )}
-      {showSidebar ? (
-        <aside className='bg-gray-300 rounded-lg p-5 w-1/3 xl:w-1/4 h-full relative'>
+      {showSidebar && (
+        <aside className='bg-secondary p-5 lg:p-7 w-1/4 xl:w-1/5 h-full relative'>
           <button
-            className='absolute top-0 right-0 bg-slate-400 px-1.5 py-5 rounded-l-md text-white'
+            className='absolute top-5  right-0 bg-accent px-1.5 py-4 rounded-l-md'
             onClick={() => setShowSidebar(!showSidebar)}
           >
             {'<<'}
           </button>
-          {menus.map((menu) => (
-            <Link
-              className='text-lg'
-              key={menu.id}
-              href={menu.path}
-            >
-              {menu.label}
-              <br />
-            </Link>
-          ))}
+          <ul className='space-y-4 list-none'>
+            {menus.map((menu) => (
+              <li key={menu.id}>
+                <Link
+                  className={path === menu.path ? 'underline' : ''}
+                  href={menu.path}
+                >
+                  {menu.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </aside>
-      ) : (
-        ''
       )}
     </>
   )
