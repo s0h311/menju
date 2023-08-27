c33562e8d642ade255f33532ddcc6e6cb9d57d91
Soheil Nazari <113988347+s0h311@users.noreply.github.com>
2023-08-27 18:34:23 +0200

    Update utility.type.ts

--- app/types/utility.type.ts
+++ app/types/utility.type.ts
@@ -1,6 +1,7 @@
 import { StorageError } from '@supabase/storage-js/dist/module/lib/errors'
 import { z } from 'zod'
 
+//TODO can be replaced when this PR to supabase/storage-js is merged: https://github.com/supabase/storage-js/pull/177
 export type StorageUploadResponse =
   | {
       data: { path: string }
