542b19a2784bd7ae3bce01cdf0801d649641db82
Soheil Nazari <kontakt@soheilnazari.de>
2023-09-05 06:56:14 +0200

    clean up store

--- store/menuStore.ts
+++ store/menuStore.ts
@@ -2,6 +2,9 @@ import { Dish, DishCategory, DishesByCategory } from '@/types/dish.type'
 import { FilterChipModel } from '@/types/filter-chip.types'
 import { create } from 'zustand'
 import { createJSONStorage, persist } from 'zustand/middleware'
+import { addDish, removeDish, updateDish } from './menuStore/dish.store'
+import { addDishCategory, removeDishCategory, updateDishCategory } from './menuStore/dishCategory.store'
+import { filter, setAllDishes, setVisibleDishes, updateFilter } from './menuStore/filter.store'
 
 export type MenuState = {
   allDishes: DishesByCategory[]
@@ -19,121 +22,24 @@ export type MenuState = {
   removeDish: (dishCategoryId: number, dishId: number) => void
 }
 
-const updateFilter = (filter: FilterChipModel, activeFilter: FilterChipModel[]): FilterChipModel[] => {
-  const existingFilter = activeFilter.find((f) => f.label === filter.label)
-  if (existingFilter) {
-    return activeFilter.filter((f) => f.label !== filter.label)
-  } else {
-    return [...activeFilter, filter]
-  }
-}
-
-const applyFilter = (dishes: DishesByCategory[], activeFilter: FilterChipModel[]): DishesByCategory[] =>
-  dishes.map((category) => {
-    const filteredDishes = category.dishes.filter(
-      (dish) => isAttributeInFilters(dish.labels, activeFilter) || isAttributeInFilters(dish.allergies, activeFilter)
-    )
-    return { ...category, dishes: filteredDishes }
-  })
-
-const isAttributeInFilters = (attribute: string[], filters: FilterChipModel[]): boolean => {
-  let attributeFound = true
-  filters.forEach((filter: FilterChipModel) => {
-    if (!attribute.includes(filter.label)) {
-      attributeFound = false
-    }
-  })
-  return attributeFound
-}
-
 export const useMenuStore = create(
   persist<MenuState>(
     (set, get) => ({
       allDishes: [],
       visibleDishes: [],
       activeFilter: [],
-      updateFilter: (filter: FilterChipModel) =>
-        set(() => ({
-          activeFilter: updateFilter(filter, get().activeFilter),
-        })),
+      updateFilter: (filter: FilterChipModel) => updateFilter(get, set, filter),
+      filter: () => filter(get, set),
+      setAllDishes: (dishes: DishesByCategory[]) => setAllDishes(get, set, dishes),
+      setVisibleDishes: (dishes: DishesByCategory[]) => setVisibleDishes(get, set, dishes),
 
-      filter: () =>
-        set(() => ({
-          visibleDishes: applyFilter(get().allDishes, get().activeFilter),
-        })),
+      addDishCategory: (dishCategory: DishCategory) => addDishCategory(get, set, dishCategory),
+      updateDishCategory: (dishCategory: DishCategory) => updateDishCategory(get, set, dishCategory),
+      removeDishCategory: (dishCategoryId: number) => removeDishCategory(get, set, dishCategoryId),
 
-      setAllDishes: (dishes: DishesByCategory[]) => {
-        set(() => ({
-          allDishes: dishes,
-        }))
-      },
-
-      setVisibleDishes: (dishes: DishesByCategory[]) =>
-        set(() => ({
-          visibleDishes: dishes,
-        })),
-
-      addDishCategory: (dishCategory: DishCategory) =>
-        set(() => ({
-          allDishes: [
-            ...get().allDishes,
-            {
-              category: dishCategory,
-              dishes: [],
-            },
-          ],
-        })),
-
-      updateDishCategory: (dishCategory: DishCategory) => {
-        const dishesByCategory = get().allDishes.find((dbc) => dbc.category.id === dishCategory.id)
-        if (dishesByCategory) {
-          dishesByCategory.category = dishCategory
-        }
-        set(() => ({
-          allDishes: get().allDishes,
-        }))
-      },
-
-      removeDishCategory: (dishCategoryId: number) =>
-        set(() => ({
-          allDishes: get().allDishes.filter((dishesByCategory) => dishesByCategory.category.id !== dishCategoryId),
-        })),
-
-      addDish: (dish: Dish) => {
-        const dishesByCategory = get().allDishes.find((dbc) => dbc.category.id === dish.categoryId)
-        if (dishesByCategory) {
-          dishesByCategory.dishes.push(dish)
-        }
-        set(() => ({
-          allDishes: get().allDishes,
-        }))
-      },
-
-      updateDish: (dish: Dish) => {
-        const dishesByCategory = get().allDishes.find((dbc) => dbc.category.id === dish.categoryId)
-        const dishIndex = dishesByCategory?.dishes.findIndex((oldDish) => oldDish.id === dish.id)
-
-        if (dishesByCategory && dishIndex && dishIndex >= 0) {
-          dishesByCategory.dishes.splice(dishIndex, 1, dish)
-        }
-
-        set(() => ({
-          allDishes: get().allDishes,
-        }))
-      },
-
-      removeDish: (dishCategoryId: number, dishId: number) => {
-        const dishesByCategory = get().allDishes.find((dbc) => dbc.category.id === dishCategoryId)
-        const dishIndex = dishesByCategory?.dishes.findIndex((oldDish) => oldDish.id === dishId)
-
-        if (dishesByCategory && dishIndex && dishIndex >= 0) {
-          dishesByCategory.dishes.splice(dishIndex, 1)
-        }
-
-        set(() => ({
-          allDishes: get().allDishes,
-        }))
-      },
+      addDish: (dish: Dish) => addDish(get, set, dish),
+      updateDish: (dish: Dish) => updateDish(get, set, dish),
+      removeDish: (dishCategoryId: number, dishId: number) => removeDish(get, set, dishCategoryId, dishId),
     }),
     {
       name: 'menu',
---
+++ store/menuStore/dish.store.ts
@@ -0,0 +1,38 @@
+import { Dish } from '@/types/dish.type'
+import { MenuState } from '@/store/menuStore'
+
+export type getState = () => MenuState
+export type setState = (
+  partial: MenuState | Partial<MenuState> | ((state: MenuState) => MenuState | Partial<MenuState>),
+  replace?: boolean | undefined
+) => void
+
+export const addDish = (get: getState, set: setState, dish: Dish) => {
+  const dishesByCategory = get().allDishes.find((dbc) => dbc.category.id === dish.categoryId)
+  if (dishesByCategory) {
+    dishesByCategory.dishes.push(dish)
+  }
+  set({ allDishes: get().allDishes })
+}
+
+export const updateDish = (get: getState, set: setState, dish: Dish) => {
+  const dishesByCategory = get().allDishes.find((dbc) => dbc.category.id === dish.categoryId)
+  const dishIndex = dishesByCategory?.dishes.findIndex((oldDish) => oldDish.id === dish.id)
+
+  if (dishesByCategory && dishIndex && dishIndex >= 0) {
+    dishesByCategory.dishes.splice(dishIndex, 1, dish)
+  }
+
+  set({ allDishes: get().allDishes })
+}
+
+export const removeDish = (get: getState, set: setState, dishCategoryId: number, dishId: number) => {
+  const dishesByCategory = get().allDishes.find((dbc) => dbc.category.id === dishCategoryId)
+  const dishIndex = dishesByCategory?.dishes.findIndex((oldDish) => oldDish.id === dishId)
+
+  if (dishesByCategory && dishIndex && dishIndex >= 0) {
+    dishesByCategory.dishes.splice(dishIndex, 1)
+  }
+
+  set({ allDishes: get().allDishes })
+}
