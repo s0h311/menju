c1f3ce50a45d2722ecf6d71f487e908a4e78251e
Soheil Nazari <kontakt@soheilnazari.de>
2023-09-20 07:04:54 +0200

    MENJU-61 | A | middleware for admin dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,13 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextRequest, NextResponse } from 'next/server'
-import { RouteKitchenDashboard, RouteLogin } from './types/routes'
+import { RouteAdminDashboard, RouteKitchenDashboard, RouteLogin } from './types/routes'
 
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone()
   const path = req.nextUrl.pathname
   const res = NextResponse.next()
 
+  //  KITCHEN //
   if (path.includes(RouteKitchenDashboard)) {
     const supabase = createMiddlewareClient({ req, res })
     const {
@@ -18,6 +19,22 @@ export async function middleware(req: NextRequest) {
       return NextResponse.redirect(url)
     }
     if (path === RouteKitchenDashboard && !session) {
+      url.pathname = RouteLogin
+      return NextResponse.redirect(url)
+    } else return res
+  }
+
+  // ADMIN //
+  if (path.includes(RouteAdminDashboard)) {
+    const supabase = createMiddlewareClient({ req, res })
+    const {
+      data: { session },
+    } = await supabase.auth.getSession()
+
+    const role = session?.user.role ?? 'notAdmin'
+    const allowedRoles = ['app_admin', 'app_superadmin']
+
+    if (!session || !allowedRoles.includes(role)) {
       url.pathname = RouteLogin
       return NextResponse.redirect(url)
     } else return res
--- types/adminUser.type.ts
+++ types/adminUser.type.ts
@@ -1,15 +1,18 @@
 import { z } from 'zod'
 
-export const zAdminUser = z.object({
-  id: z.string().optional(),
-  name: z.string().min(2).max(20),
-  email: z.string().email(),
-  role: z.enum(['Admin', 'Super Admin']),
-})
+export const zRoles = z.enum(['Admin', 'Super Admin'])
 
 const zPassword = z.object({
   password: z.string().min(8).max(20),
 })
+
+export const zAdminUser = z
+  .object({
+    id: z.string().optional(),
+    name: z.string().min(2).max(20),
+    email: z.string().email(),
+  })
+  .merge(z.object({ roles: zRoles }))
 
 export const zRegisterCredentialsAdminUser = zAdminUser.merge(zPassword)
 
