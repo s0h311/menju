e36419914962509f090ccdbc1061d7f41e231c95
Soheil Nazari <kontakt@soheilnazari.de>
2023-10-01 07:56:39 +0200

    force-dynamic added to orders page

--- app/kitchen/(dashboard)/orders/page.tsx
+++ app/kitchen/(dashboard)/orders/page.tsx
@@ -2,6 +2,8 @@ import OrderList from '@/components/kitchen/orderList'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Orders() {
   const superbaseClient = createServerComponentClient({ cookies })
   const { data } = await superbaseClient.from('order').select()
