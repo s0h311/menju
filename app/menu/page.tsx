7c4ad01b654a17ed922a2e9630efad8a98961cd5
Soheil Nazari <kontakt@soheilnazari.de>
2023-10-04 07:09:34 +0200

    Typed the Query Params

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -13,11 +13,12 @@ import React from 'react'
 import CartDialog from '@/components/guest/cartDialog'
 import useDish from '@/hooks/useDish'
 import { useSearchParams } from 'next/navigation'
+import { QUERY_PARAM } from '@/types/queryParams.type'
 
 export default function Menu() {
   const queryParams = useSearchParams()
-  const restaurantId: number = parseInt(queryParams.get('r') ?? '')
-  const tableId: string = queryParams.get('t') ?? 'unknown'
+  const restaurantId: number = parseInt(queryParams.get(QUERY_PARAM.restaurandId) ?? '')
+  const tableId: string = queryParams.get(QUERY_PARAM.tableId) ?? 'unknown'
 
   const { dishesByCategory, visibleDishes } = useDish({ restaurantId, language: 'de', tableId })
 
