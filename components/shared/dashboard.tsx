ba45de10bb4a44e36abc33c031f3bcdf8ff7545b
FarhamTehrani <128404074+FarhamTehrani@users.noreply.github.com>
2023-10-23 14:18:01 +0200

    sidebar change

--- components/shared/dashboard.tsx
+++ components/shared/dashboard.tsx
@@ -14,7 +14,7 @@ export default function Dashboard({ sidebarMenus, children }: DashboardProps) {
     <main className='grid place-items-center h-screen w-screen'>
       <section className='flex w-full h-full'>
         <Sidebar menus={sidebarMenus} />
-        <div className='space-y-3 w-full h-full rounded-lg p-5 xl:p-10 '>{children}</div>
+        <div className='space-y-3 w-full h-full rounded-lg p-10 xl:p-10 '>{children}</div>
       </section>
     </main>
   )
