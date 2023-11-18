4194e6f73012069df56ba282143bcc50a9bb2a02
maxiirs <maximilian.buchmann@haw-hamburg.de>
2023-11-18 15:27:54 +0100

    added color settings and support for the 4 major colors

--- components/kitchen/colorSettings.tsx
+++ components/kitchen/colorSettings.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { Button } from '@mui/material'
-import { theme } from '@/ui/theme'
 import ThemeProvider from '@mui/material/styles/ThemeProvider'
 import { type Colors } from '@/types/restaurant.type'
 import { trpc } from '@/trpc/trpc'
@@ -8,26 +7,42 @@ import toast from '@/utils/toast'
 import React, { useState } from 'react'
 import { GithubPlacement } from '@uiw/react-color-github'
 import Chrome from '@uiw/react-color-chrome'
+import useStore from '@/hooks/useStore'
+import { useRestaurantStore } from '@/store/restaurantStore'
+import { useCustomTheme } from '@/ui/theme'
 
-type ColorSettingProps = {
-  restaurantId: number
-  colors: Colors
-}
-export default function ColorSettings({ restaurantId, colors }: ColorSettingProps) {
+export default function ColorSettings() {
+  const theme = useCustomTheme()
+  const restaurantStore = useStore(useRestaurantStore, (state) => state)
   const { mutateAsync: updateColorsMutation } = trpc.updateColors.useMutation()
   const [valueName, setValueName] = useState<string>('')
   const [colorValue, setColorValue] = useState<string>('')
 
-  const save = async (newColors: Colors): Promise<void> => {
-    updateColorsMutation(
-      {
-        restaurantId: restaurantId,
-        colors: newColors,
-      },
-      {
-        onSuccess: displayErrorOrSuccess,
+  const save = async (newColor: string): Promise<void> => {
+    try {
+      if (!restaurantStore) {
+        throw new Error()
       }
-    )
+      const currentColors = restaurantStore.colors
+      const mergedColors = {
+        ...currentColors,
+        [valueName]: newColor,
+      }
+      const updatedColor = await updateColorsMutation(
+        {
+          restaurantId: restaurantStore.restaurantId,
+          colors: mergedColors,
+        },
+        {
+          onSuccess: displayErrorOrSuccess,
+        }
+      )
+      restaurantStore.setColors(updatedColor.colors as Colors)
+      setColorValue('')
+      setValueName('')
+    } catch (error) {
+      toast.error('Fehler beim Speichern der Farben ')
+    }
   }
 
   const displayErrorOrSuccess = (): void => {
@@ -35,85 +50,96 @@ export default function ColorSettings({ restaurantId, colors }: ColorSettingProp
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <section style={{ display: 'flex', gap: '20px' }}>
-        <form className='grid gap-5 w-fit'>
-          <h2 className='text-lg'>Farben</h2>
+    restaurantStore &&
+    restaurantStore.colors && (
+      <ThemeProvider theme={theme}>
+        <section style={{ display: 'flex', gap: '20px' }}>
+          <form className='grid gap-5 w-fit'>
+            <h2 className='text-lg'>Farbeinstellungen</h2>
 
-          <label className='-mb-4'>Hauptfarbe</label>
-          <Button
-            sx={{ width: '100%' }}
-            variant='outlined'
-            color='primary'
-            onClick={() => {
-              setValueName('primary')
-              setColorValue(colors.primary)
-            }}
-          >
-            Bearbeiten
-          </Button>
-
-          <label className='-mb-4'>Sekundärfarbe</label>
-          <Button
-            sx={{ width: '100%' }}
-            variant='outlined'
-            color='secondary'
-            onClick={() => {
-              setValueName('secondary')
-              setColorValue(colors.secondary)
-            }}
-          >
-            Bearbeiten
-          </Button>
-
-          <label className='-mb-4'>Akzentfarbe</label>
-          <Button
-            sx={{ width: '100%' }}
-            variant='outlined'
-            color='accent'
-            onClick={() => {
-              setValueName('accent')
-              setColorValue(colors.accent)
-            }}
-          >
-            Bearbeiten
-          </Button>
-
-          <label className='-mb-4'>Textfarbe</label>
-          <Button
-            sx={{ width: '100%' }}
-            variant='outlined'
-            color='textColor'
-            onClick={() => {
-              setValueName('textColor')
-              setColorValue(colors.textColor)
-            }}
-          >
-            Bearbeiten
-          </Button>
-        </form>
-        {colorValue !== '' && valueName !== '' && (
-          <div style={{ display: 'flex', flexDirection: 'column', marginTop: 50 }}>
-            <Chrome
-              color={colorValue}
-              style={{ float: 'left' }}
-              placement={GithubPlacement.Right}
-              onChange={(color) => {
-                setColorValue(color.hexa)
-              }}
-            />
+            <label className='-mb-4'>Hauptfarbe</label>
             <Button
+              sx={{ width: '100%' }}
+              variant='outlined'
+              color='primary'
               onClick={() => {
-                setColorValue('')
-                setValueName('')
+                setValueName('primary')
+                setColorValue(restaurantStore.colors!.primary)
               }}
-              color={valueName}
             >
-              Speichern
+              Bearbeiten
             </Button>
-          </div>
-        )}
-      </section>
-    </ThemeProvider>
+
+            <label className='-mb-4'>Sekundärfarbe</label>
+            <Button
+              sx={{ width: '100%' }}
+              variant='outlined'
+              color='secondary'
+              onClick={() => {
+                setValueName('secondary')
+                setColorValue(restaurantStore.colors!.secondary)
+              }}
+            >
+              Bearbeiten
+            </Button>
+
+            <label className='-mb-4'>Akzentfarbe</label>
+            <Button
+              sx={{ width: '100%' }}
+              variant='outlined'
+              color='accent'
+              onClick={() => {
+                setValueName('accent')
+                setColorValue(restaurantStore.colors!.accent)
+              }}
+            >
+              Bearbeiten
+            </Button>
+
+            <label className='-mb-4'>Textfarbe</label>
+            <Button
+              sx={{ width: '100%' }}
+              variant='outlined'
+              color='textColor'
+              onClick={() => {
+                setValueName('textColor')
+                setColorValue(restaurantStore.colors!.textColor)
+              }}
+            >
+              Bearbeiten
+            </Button>
+          </form>
+          {colorValue !== '' && valueName !== '' && (
+            <div style={{ display: 'flex', flexDirection: 'column', marginTop: 50 }}>
+              <Chrome
+                color={colorValue}
+                style={{ float: 'left' }}
+                placement={GithubPlacement.Right}
+                onChange={(color) => {
+                  setColorValue(color.hexa)
+                }}
+              />
+              <Button
+                color='success'
+                onClick={() => {
+                  save(colorValue)
+                }}
+              >
+                Speichern
+              </Button>
+              <Button
+                color='warning'
+                onClick={() => {
+                  setColorValue('')
+                  setValueName('')
+                }}
+              >
+                Verwerfen
+              </Button>
+            </div>
+          )}
+        </section>
+      </ThemeProvider>
+    )
   )
 }
