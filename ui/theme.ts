d2d956a8cf54cfb5c3c2e3ece42a0c124bc17c33
maxiirs <maximilian.buchmann@haw-hamburg.de>
2023-11-18 15:28:51 +0100

    made theme dynamic

--- ui/theme.ts
+++ ui/theme.ts
@@ -1,5 +1,6 @@
 import createTheme from '@mui/material/styles/createTheme'
 import type { PaletteColorOptions } from '@mui/material/styles/createPalette'
+import useRestaurant from '@/hooks/useRestaurant'
 
 declare module '@mui/material/styles' {
   interface Palette {
@@ -54,22 +55,25 @@ declare module '@mui/material/SvgIcon' {
   }
 }
 
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#7eaa92',
+export const useCustomTheme = () => {
+  const { colors } = useRestaurant()
+  return createTheme({
+    palette: {
+      primary: {
+        main: colors ? colors.primary : '#7eaa92',
+      },
+      secondary: {
+        main: colors ? colors.secondary : '#caddc3',
+      },
+      accent: {
+        main: colors ? colors.accent : '#f7e987',
+      },
+      bgColor: {
+        main: '#f4f4f4',
+      },
+      textColor: {
+        main: colors ? colors.textColor : '#445069',
+      },
     },
-    secondary: {
-      main: '#caddc3',
-    },
-    accent: {
-      main: '#f7e987',
-    },
-    bgColor: {
-      main: '#f4f4f4',
-    },
-    textColor: {
-      main: '#445069',
-    },
-  },
-})
+  })
+}
